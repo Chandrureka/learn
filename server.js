ae25dbf3ce0516f6a63391adb0a23f68bb90a174
chandru-pt7603 <chandru-pt7603@csez.zohocorpin.com>
2024-07-30 13:11:30 +0530

    fourth commit

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ const server=http.createServer((req,res)=>{
    
 });
  server.listen(3000,'localhost',()=>{
-    console.log("serverrr iss listening on port 3000");
+    console.log("serverrr iss listeningg on port 3000");
  });
 
